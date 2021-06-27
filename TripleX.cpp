af5677c676a26589394a99e95077dd776f567072
Nathan Flessner <nathanflessner1@gmail.com>
2021-06-27 13:15:28 -0600

    added cpp and executible

---
+++ TripleX.cpp
@@ -0,0 +1,132 @@
+#include <iostream>
+#include <ctime> 
+
+void IntroImage()
+{
+    std::cout << "                                            .......                                        ............\n";                                            
+    std::cout << "                                    ..,:ldxkO00KK0OOxdl:,.                           .',:ldkO0KXXXXXX0Oxoc,.\n";                                       
+    std::cout << "                                 .;okKXNNXKK000O00KKXNWWNKkl;.                    .:okKXNNNNWWWWNNNNNNWWWWX0xl;.\n";                                   
+    std::cout << "                              .:x0XKOkdlc;,,,''''''',;cldOKNN0d:.              .:dOKXNWWWN0kxdollllllcc:clok0XX0d;.\n";                                
+    std::cout << "                            'lOK0kl:,',;:ccclllllllcc:,''';ldOXX0o,          'cxOKXXNX0xolllloodddddddoc:,'.';oOXXOc.\n";                              
+    std::cout << "                          'oO0ko:;,:loool:;,,''..'',;:clllc:,;cdOXKd,.     .:dxk0KKOoccllc:,'.......'',;cool:,..:xXNO:.\n";                            
+    std::cout << "                        .cO0kl;,:lxxo;..               ..,:llc:;:lxKKx:...,looxkkdc;:c;'.                .';lolc,.:kNXd'\n";                           
+    std::cout << "                       'd00d;,:ldxl'                       .':ll:,,cxO00Okdllloc,,;;.                       .'cloc,,dKXO;\n";                          
+    std::cout << "                      'kXOl;;cdxc.                            .:l:;;:dKXXXXkc;,',,.                           .,clo:,c0XO;\n";                         
+    std::cout << "                     .xXOl,;oxx;                               .ldlc;;xKNK0Kd.';.                               .:odc,cOXk'\n";                        
+    std::cout << "                    .lK0l,;okd'                                 ,xxc,';d0OxxOc..                                 .cooc,l0Ko.\n";                       
+    std::cout << "                    ,OKd;,lkk;                                   :x:,'.;oxlcxl.                                   'coo;;x0O;\n";                       
+    std::cout << "                    c0Oc';dOl.                                   .::,'.',:occ:                                    .;lo:,lO0l.\n";                      
+    std::cout << "                   .o0x;,lOO;                                     .;,..',,cl:;.                                    'ldc,:kOo.\n";                      
+    std::cout << "                   'd0d,;d0O,                                 ... .:,'.',;:clc,.....                               'ddl,:xko.\n";                      
+    std::cout << "                   .dOd,;xK0,                           ......... .'.,,'':o::;'........                            ,xdc,;xOl.\n";                      
+    std::cout << "                   .cOx;,oKXc                        ..''.......... .;c,.;loc,.............                        lOd:':Ok:.\n";                      
+    std::cout << "                    ;kkc,c0Nk'                    .''''....''''......,c:;'cxxo,...............                    ,kOo,.cOx,\n";                       
+    std::cout << "                    .lkx;,xXXd.                 .,;'...''''',,'''... .clc,,d00x;....'''''....''.                 .dKk:.;xkl.\n";                       
+    std::cout << "                     'dOo':kNXd.              .,;'.',,'',,,,,,,,'.... 'llc,;kXXk:'...',''......''.              .dX0o',dko.\n";                        
+    std::cout << "                      ,xOl,;xNNk,            ,:,',;;;;;;,;;;,,''..    .,clo;;xXX0o,',,,',''''''..''.          .:ONKd,;oko'\n";                         
+    std::cout << "                       ,dxl,'oKNXd'        .cc;;::::;;;,,::;,,'...   ....'ll:,l0NXOo:;,,,'',,'''..';.       .,xXWKl;cdxc.\n";                          
+    std::cout << "                        .cdl;':xXNXx:.    ,lc:cccccc:::cc:;;;;,'......  . .cdl;;oKNXOo;,,,,,;;,',,'';,   .'lkXWKx:;odo;.\n";                           
+    std::cout << "                         .,loc;'cx0NNKkl:lxl:llccllcllolcc:;,'....'..   .  .:dxl,;oOXN0o,':;,;;,,,,'';;:dOXNN0d:;cdo:.\n";                             
+    std::cout << "                          .,cll:;:lxOKNWNX0Okkkxxxxxddl:;,'...',,.    ..    .cdxo:;cdxl,..::,,,,;;;,,:dKKOdc;;ldoc'\n";                               
+    std::cout << "                             .':loc;,;codxkkkkkkxdolc:;,'.',,;:;'.     ...     .;oxoc:;,''..;;,,,;;::,'coc::cooo:.\n";                                 
+    std::cout << "                                .,cooolc:;;::::;;;;,;;:::llc:,.       ..,.       .,;:oxxl:'..;c,,;::::,:odool:'.\n";                                   
+    std::cout << "                                   .';ldxkkxxxxxxxxxxddl:,..   ..  .',;::;,,..  ..   .':cc:;';c:;:cccc:,cc'.\n";                                       
+    std::cout << "                                      .'',;:cclllc;,'..          .;:;;::::;;:;,.            ..;c:cllllc,::.\n";                                        
+    std::cout << "                                      .'..........              .,;:,,;;;;,;::;'              'llcoooc:';c.\n";                                       
+    std::cout << "                                      ',....'.'''.              ';;'.':llc;',;;,.             .llloolc:,;l'\n";                                        
+    std::cout << "                                      ,;...''''','             ..,;'..';;;'.',,,..            'olloolcc,:o.\n";                                        
+    std::cout << "                                      ,:'',,,;;,;;. ..          .','........'''.     .......  ;ollllcc:,cl.\n";                                        
+    std::cout << "                                     'c;,,,,;;,::'.              ......''.....              .lollccc:;,c:.\n";                                        
+    std::cout << "                                      .cl,;;,;;;;::.                .......                  ,ollllc;;',:.\n";                                         
+    std::cout << "                                       ,lc;;,,,;,;c,.                                       ,llllll:;,.;;.\n";                                         
+    std::cout << "                                       .:l:,,,,,,';:,.                        .            ,lc:ccc::;';c.\n";                                          
+    std::cout << "                                        .cc,''',,,',:;.          .             .         .;c:::::::;,;l'\n";                                           
+    std::cout << "                                         .:c,,,',,'';::,.      ..               .       .;:;;::::c:;co,\n";                                            
+    std::cout << "                                          .::;,,,,',;;,,,'..   .                ..   .';:,,::;;:c::lo'\n";                                             
+    std::cout << "                                            ':;,',,,,,,''',,....                 .'',;:::;;:::cc;:oc.\n";                                              
+    std::cout << "                                             .,:;'''''''''''..,,,'............',,;::;;:::cccccccol,\n";                                                
+    std::cout << "                                               .,,'..'..'''....',;;;;::::::::cc;,,;;;;;:::cccclo;.\n";                                                 
+    std::cout << "                                                 .',''..''....,,;,,,;;:;;;::;;:;;;;;,,;;,,;ccc,.\n";                                                   
+    std::cout << "                                                  ..''''....',,;;,,;;;;;;::;,,;;;,,'',;;::;.\n";                                                      
+    std::cout << "                                                      ..'''...'';;;;;;;;;;;;,,,'''',;;;;,.\n";                                                         
+    std::cout << "                                                           ...,,:c::::::::::;;,,,,''...\n";                                                            
+    std::cout << "                                                                ...''',,,,'''...\n";                                                                                                                                                                                                                     
+    std::cout << "                                                Welcome to TripleX, Wheel of Time edition\n";
+}
+
+void PrintIntroduction(int Difficulty)
+{                                                                                                    
+
+    std::cout << "\n\nYou are an Aes Sedai who needs to break into level " << Difficulty;
+    std::cout << " of the vault holding the Ter'angreal!\nYou need to use the one power to access the correct code to enter...\n";
+}
+//end entrance statement
+bool PlayGame(int Difficulty)
+{
+    
+    PrintIntroduction(Difficulty);
+
+
+    const int CodeA = rand() % Difficulty + Difficulty;
+    const int CodeB = rand() % Difficulty + Difficulty;
+    const int CodeC = rand() % Difficulty + Difficulty;
+//end 3 number code daclerations
+
+    const int CodeSum = CodeA + CodeB + CodeC;
+    const int CodeProduct = CodeA * CodeB * CodeC;
+
+//Level 1 Statements    
+    std::cout <<std::endl;
+    std::cout << "* There are 3 numbers in the code...\n";
+    std::cout << "* The codes add-up to: " << CodeSum << std::endl;
+    std::cout << "* The codes multiply to give: " << CodeProduct << std::endl;
+    std::cout << "* Please enter 1 numeric character followed by enter, or each guess seperated by a space.\n";
+
+//Declare player Guess
+    int GuessA, GuessB, GuessC;
+    std::cin >> GuessA >> GuessB >> GuessC;
+
+    
+    int GuessSum = GuessA + GuessB + GuessC;
+    int GuessProduct = GuessA * GuessB * GuessC;
+
+//Check Player answers
+    if (GuessSum == CodeSum && GuessProduct == CodeProduct)
+    {
+        std::cout << "\nSuccess! Your Power Increases. Attempt the next lock! ***";
+        return true;
+    }
+    else
+    {
+        std::cout << "\nFail! You will be stilled if failure continues. Try again! ***\n";
+        return false;
+    }
+}
+int main()
+{
+    srand(time(NULL)); // create new random sequence based on time of day
+
+    int LevelDifficulty = 1;
+
+    int const MaxDifficulty = 5;
+
+    if (LevelDifficulty = 1)
+    {
+        IntroImage();
+    }
+
+    while (LevelDifficulty <= MaxDifficulty) //Loop game until all levels completed
+    {
+        bool bLevelComplete = PlayGame(LevelDifficulty);
+        std::cin.clear(); //Clears any errors
+        std::cin.ignore(); //Discards the buffer
+
+        if (bLevelComplete)
+        {
+            ++LevelDifficulty;
+        }
+        
+    }
+    std::cout << "\n*** Fantastic work Aes Sedai. You have accessed the Ter'angreal. Remember to set your price...";
+    return 0;
+}
